61259df2bdce658403a453a17a97bf25cc1fd20e
lemon-lexiaobai <17743692127@163.com>
2020-07-02 14:20:17 +0800

    6、jsonp不是ajax

---
+++ practice/6/index.js
@@ -0,0 +1,22 @@
+/**
+ * jsonp不是真正的ajax
+ *
+ * 浏览器的同源策略（服务器端没有同源策略）和跨域
+ *
+ * img和script标签能绕过跨域
+ * */
+
+/**
+ * document load与ready的区别
+ *
+ * load： 页面的全部资源加载完才会执行，包括图片、视频等  load
+ * ready： DOM渲染完即可执行，此时图片、视频可能还没有加载完  DOMContentLoaded
+ * */
+
+/**
+ * == 和 === 的区别
+ * == 会尝试类型转换
+ * === 严格相等
+ *
+ * ==null 相当于 ===null||===undefined
+ * */
