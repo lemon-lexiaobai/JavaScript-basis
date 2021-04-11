8b3c3ae530f137248284cbaab0bc04a093941ffd
lemon-lexiaobai <17743692127@163.com>
2021-04-11 21:05:21 +0800

    2021/4/11 防抖

--- drawing/debounce.js
+++ drawing/debounce.js
@@ -30,3 +30,19 @@ function debounce(fn,delay = 500){
 input1.addEventListener('keyup',debounce(()=>{
     console.log(input1.value)
 }))
+
+
+/**
+ * 高频触发时，n秒函数只会执行一次，如果n秒内高频触发时间再次触发，则会重新计算时间
+ * 常用于用户进行搜索输入节约请求资源，window触发resize事件时进行防抖只触发一次
+ * */
+const debounceCopy = (fn, time = 100) => {
+    let timer = null
+    return function(){
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+            fn.apply(this, arguments)
+            timer = null
+        }, time)
+    }
+}
