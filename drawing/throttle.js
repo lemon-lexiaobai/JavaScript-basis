a49fcfc8bfe140f2c74659209a4fd43d3a9831c8
lemon-lexiaobai <17743692127@163.com>
2021-04-11 20:59:08 +0800

    2021/4/11 节流

--- drawing/throttle.js
+++ drawing/throttle.js
@@ -29,3 +29,19 @@ function throttle(fn,delay=100){
 div1.addEventListener('drag',throttle(function(e){
     console.log(e.offsetX,e.offsetY)
 }))
+
+/**
+ * 节流，短时间高频触发，但指定时间内只会执行一次，会稀释函数的执行频率
+ * */
+
+const throttleCopy = (fn, time = 100) => {
+    let timer = null
+    return function(){
+        if(timer) return
+        // setTimeout返回值是一个整数
+        timer = setTimeout(() => {
+            fn.apply(this, arguments)
+            timer = null
+        }, time)
+    }
+}
